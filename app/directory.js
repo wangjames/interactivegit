feec1e196afadc38a38ae8beb97dc7cf71667292
wangjames <wang.james.y@gmail.com>
2017-05-15 22:50:12 +0000

    created component with console and tree visualization

---
+++ app/directory.js
@@ -0,0 +1,82 @@
+
+
+module.exports.directoryObject = function DirectoryObject()
+{
+    function Folder(name)
+    {
+      this.directory_name = name;
+      this.children = []
+      this.files = []
+      this.parentNode = null
+    };
+    
+    function TextObject(file_name)
+    {
+        this.name = file_name;
+        this.text = "";
+    }
+  this.currentPointer = new Folder("root")
+  
+  this.traverseToChild = function(name)
+  {
+    var children_array = this.currentPointer.children;
+    function findChild(child) { 
+      return child.directory_name === name;
+    }
+    var new_pointer = children_array.find(findChild);
+    if (new_pointer != null)
+    {
+      this.currentPointer = new_pointer;
+      return;
+    }
+    else
+    {
+      return;
+    }
+    
+  }
+  
+  this.traverseBackwards = function()
+  {
+    this.currentPointer = this.currentPointer.parentNode;
+  }
+
+  this.createFolder = function(directory_name)
+  {
+    var new_folder = new Folder(directory_name);
+    if (!((this.currentPointer.children.indexOf(new_folder)) > -1))
+    {
+        new_folder.parentNode = this.currentPointer;
+        this.currentPointer.children.push(new_folder)
+    }
+  }
+
+  this.createFile = function(file_name)
+  {
+    var new_file = new TextObject(file_name);
+    if ((this.currentPointer.files.includes(file_name)) > -1)
+    {
+        this.currentPointer.files.push(new_file)
+    }
+  }
+
+  this.displayContents = function()
+  {
+    for (var key in this.currentPointer.children)
+    {
+      console.log(key);
+    }
+
+    for (var key in this.currentPointer.files)
+    {
+      console.log(key);
+    }
+  }
+
+  this.displayCurrentPointer = function()
+  {
+    console.log(this.currentPointer);
+  }
+  
+  
+}
--- app/index.js
+++ app/index.js
@@ -1,12 +1,69 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-require('./index.css');
+var directoryObject = require('./directory');
+var JQConsole = require('./jqconsole');
 
-class App extends React.Component {
-    render() 
+require("./index.css");
+
+var Visualization = React.createClass({
+    
+    
+    renderChildren: function(item)
     {
-        return <div>Hello World</div>
+       return <Visualization directory={item}/>
+    },
+    render: function()
+    {
+        var item = this.props.directory;
+        return (
+            <div>
+                <span>{item.directory_name}</span>
+                {item.children.map(this.renderChildren, this)}
+            </div>
+            )
     }
-}
+})
+var App = React.createClass({
+    componentDidMount: function()
+    {
+        $(function () {
+        var jqconsole = $('#console').jqconsole('Hi\n', '>>>');
+        var startPrompt = function () {
+          // Start the prompt with history enabled.
+          jqconsole.Prompt(true, function (input) {
+            // Output input with the class jqconsole-output.
+            jqconsole.Write(input + '\n', 'jqconsole-output');
+            // Restart the prompt.
+            startPrompt();
+          });
+        };
+        startPrompt();
+      });
+    },
+    getInitialState: function()
+    {
+        var directobject = new directoryObject.directoryObject();
+        directobject.createFolder("hi");
+        directobject.createFolder("hey");
+        directobject.traverseToChild("hey");
+        directobject.createFolder("yes");
+        directobject.traverseBackwards();
+        directobject.traverseToChild('hi');
+        directobject.createFolder("yoyoyo");
+        directobject.traverseBackwards();
+        return {directory: directobject.currentPointer};
+    },
+    render : function()
+    {
+        return (
+            <div>
+                <Visualization directory={this.state.directory}/>
+                <div id="console"></div>
+            </div>
+            )
+    
+    }
+})
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
