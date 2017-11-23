c7c06ee97f2293f2714a97ea53f50f058d54cf16
wangjames <wang.james.y@gmail.com>
2017-11-23 20:19:06 +0000

    made changes to text

--- app/textsections/LessonOne.js
+++ app/textsections/LessonOne.js
@@ -1,33 +1,41 @@
-var text_object_1 = [`With this first tutorial, we can start looking at how people can collaborate with Git. 
-As stated before in the introduction, collaboration with Git involves commits to a central repository.`,
-`Everyone receives a copy of the entire project and in order for users to collaborate on this project,
-they must send up a new version of the entire project with changes which will be meticulously tracked by Git.`]
+var text_object_1 = [`With this first section, we can start looking at how people collaborate on Git.`,
+`Imagine that you’re working on an essay with a team where each team member is responsible for a section of the essay.`
+`Immediately, we can see that there are challenges on how to coordinate different versions and how to track all of the latest changes.`]
 
-var text_object_2 = [`When a repository is first created, we have to initialize a repository with our project.`,
-`Type git init while in the root directory. Let’s do that now.`]
+var text_object_2 = [`These challenges could be solved by using Git. Git uses the idea of a repository which is essentially the folder that holds your project’s files,
+but it also tracks all changes made to that repository.`,
+`Every group will have a single repository that acts as the single source of truth for their project. This is called a remote repository.`]
 
-var text_object_3 = [`Make a file using the mkdir command. Make another file using the touch command. You can click on file to open up an editor for that file.`]
- 
-var text_object_4 = [`Now that you are ready to push up the initial commit or version of your project to the repository, you first have to prepare all of the files to be sent.
-Similar to using the attach functionality when sending an email, you have to do the same with git.`,
-`The function for this is git add. You can go file by file adding each file with each file’s name. 
-For example, if you have a file called text.txt in your current working directory,
-you can do git add text, and this will add the file to be prepared to be commited (this is also referred to as adding to the staging area).`]
- 
-var text_object_5  = [`A shortcut for this is the command git add . - when you type this in, all of the files and folders
-and their files and folders to the staging area in the current working directory.`,
-`Do this now.
-Make sure you’re in the root directory and type git add .`, 
-`If you type git status, you can see that you’ve staged these files for commit.`]
- 
-var text_object_6 = [`Now that you added the files to the staging area, we can make a commit that can be sent to the public repository.
-With every commit, we need attach a message to it.`,
-`For example, with a set of files in a commit, we can attach the message “Changed introduction of the essay.”
-To do this, we can execute the command git commit -m “Changed introduction of the essay.”`,
-`Do this now. Make a commit with your own commit message.`]
- 
-var text_object_7 = [`After doing this, you have now created a snapshot of your working directory that you can now push to a repository to share with others.
-This will be discussed in the next section.`]
+var text_object_3 = [`Once the remote repository is created, each team member downloads a copy of the remote repository to their computers.`, 
+`This is called a local repository.
+Team members work on their computer on their respective local repository and push all new changes and modifications to the remote repository.`]
+
+var text_object_4 = [`In order to create a remote repository, a user first must create a local repository that will serve as the initial version of a remote repository. `]
+
+var text_object_5 = [`First, we want to make a directory called essay. This tutorial simulates a Linux command line so the command for is mkdir essay.`]
+
+var text_object_6 = [`Then create an initial file in the essay folder.`,
+`First, change directory with the command cd essay. Then create the file using the command touch introduction.`,
+`Return back to the root with the command cd ../`]
+
+var text_object_7 = [`Now that we have these files created, we can create our Git repository.`]
+
+var text_object_8 = [`Enter the command git init. This creates a repository to watch over the current directory.`,
+`Next, we need to add these files to staging area.`]
+
+var text_object_9 = [`The staging area is where you confirm what changes you want to push up into the remote repository.
+We can add each file individually, but a shortcut is to just add each file through the git add . command`]
+
+var text_object_10 = [`Once these files have been added, we can now make a commit. 
+All commits need a message associated with them so we will use the message “initial version”.`]
+
+var text_object_11 = [`Enter the command git commit -m “initial version”.`]
+
+var text_object_12 = [`Now we have created a local repository that can serve as the remote repository.`, 
+`We can see our first initial commit using the command git log.`]
+
+var text_object_13 = [`In the next section, we will be repeating these steps and then make this local repository be our remote repository.`,
+`Please hit the back button on your browser to go back to the menu.`]
 
 var text_array = [text_object_1, text_object_2, text_object_3,text_object_4,text_object_5, text_object_6, text_object_7];
 
